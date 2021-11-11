0bb07053a26782d55338b6a20ec1c31794dd69a2
dusipaul <dusipaul@gmail.com>
2021-11-11 19:15:58 +0200

    dab connect changes

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('ftpupload.net','epiz_30323556','UdeCCviBpK','epiz_30323556_tms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','tms_db')or die("Could not connect to mysql".mysqli_error($con));
