14f7fbdc5e981e4b46d06ce20543ca0567535d00
dusipaul <dusipaul@gmail.com>
2021-11-12 12:04:12 +0200

    creadb

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php 
 
 $conn= new mysqli_connect('us-cdbr-east-04.cleardb.com','b821860059a73a','36f815a4','heroku_15411792d70b6d6');
-if !$conn{
+if (!$conn){
     die("Could not connect to mysql".mysqli_error($conn));
 }
