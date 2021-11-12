d54fd7511c7966db6565cde82cdc8c7fb68ff9ea
dusipaul <dusipaul@gmail.com>
2021-11-12 11:50:25 +0200

    creadb

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','tms_db')or die("Could not connect to mysql".mysqli_error($conn));
+$conn= new mysqli('localhost','b821860059a73a','36f815a4','heroku_15411792d70b6d6')or die("Could not connect to mysql".mysqli_error($conn));
