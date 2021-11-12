1db8eeafd709c76d26c165a5982d2a0a24a03f31
dusipaul <dusipaul@gmail.com>
2021-11-12 12:10:24 +0200

    creadb

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ include('./db_connect.php');
   // if(!isset($_SESSION['system'])){
 
     $system = $conn->query("SELECT * FROM system_settings");
-    $system->fetch_array();
+    $system->fetch_array(MYSQLI_ASSOC);
     foreach($system as $k => $v){
       $_SESSION['system'][$k] = $v;
     }
