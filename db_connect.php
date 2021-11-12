f56e9861ab7ef180aba867980bb9308be82abf2e
dusipaul <dusipaul@gmail.com>
2021-11-12 15:35:04 +0200

    grgrg

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,4 @@
 <?php 
+// $conn= new mysqli('us-cdbr-east-04.cleardb.com','b821860059a73a','36f815a4','heroku_15411792d70b6d6');
 
-$conn= new mysqli('us-cdbr-east-04.cleardb.com','b821860059a73a','36f815a4','heroku_15411792d70b6d6');
-if (!$conn){
-    die("Could not connect to mysql".mysqli_error($conn));
-}
+$conn= new mysqli('localhost','root','','tms_db')or die("Could not connect to mysql".mysqli_error($conn));
